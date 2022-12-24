b7d51feda0d0ae3074bc8f1f9d637fef9eacb557
Raúl <119669918+RaulTheViking@users.noreply.github.com>
2022-12-24 18:05:05 +0100

    Add files via upload

---
+++ Kata Variables y Condicionales (01)/code.js
@@ -0,0 +1,13 @@
+function evenOrOdd(n) {
+    if (n % 2 == 0) {
+        return "even"
+    }
+    else {
+        return "odd"
+    }
+}
+
+function greeting(name) {
+    return ("Hola, " + name + "!");
+}
+
